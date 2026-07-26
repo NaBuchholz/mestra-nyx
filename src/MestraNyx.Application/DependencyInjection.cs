aa7f815fb44f3111149ef37a65b333236e43a770
NaBuchholz <nathaliabuchh@hotmail.com>
2026-07-26 02:02:54 +0000

    feat(application): add MediatR validation pipeline

--- src/MestraNyx.Application/DependencyInjection.cs
+++ src/MestraNyx.Application/DependencyInjection.cs
@@ -1,5 +1,6 @@
 using FluentValidation;
 using MestraNyx.Application.Campaigns.Commands.CreateCampaigns;
+using MestraNyx.Application.Common.Behaviors;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace MestraNyx.Application;
@@ -17,8 +18,12 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddMediatR(configuration =>
+        {
             configuration.RegisterServicesFromAssembly(
-                typeof(DependencyInjection).Assembly));
+                typeof(DependencyInjection).Assembly);
+
+            configuration.AddOpenBehavior(typeof(ValidationBehavior<,>));
+        });
 
         services.AddScoped<
             IValidator<CreateCampaignCommand>,
